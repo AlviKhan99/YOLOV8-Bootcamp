e6cd5c4762782ff208ee098eb472d2ad5e398bd2
AlviKhan99 <mkkc1968@gmail.com>
2024-06-19 06:21:13 +0800

    added new and edited

--- tutorial1_object_detection.py
+++ tutorial1_object_detection.py
@@ -2,14 +2,15 @@ from ultralytics import YOLO
 
 # Load a pretrained YOLOv8n model
 model = YOLO('yolov8n.pt')
+# model = YOLO('best.pt')
 
 # Run inference on the source
-results = model(source='0', show=True, conf=0.4) # Live inferencing
+# results = model(source='0', show=True, conf=0.4) # Live inferencing
 
 # results = model(source='0', show=True, conf=0.4, save=True) # If you want to save the live inference in your device
 
-# results = model(source='https://www.youtube.com/watch?v=ORrrKXGx2SE', show=True, conf=0.4)
+# results = model(source='bus.jpg', show=True, conf=0.4)
 
-# results = model(source='https://www.youtube.com/watch?v=ORrrKXGx2SE', show=True, conf=0.4, save=True) # if you want to save the inference video in your device
+results = model(source='bus.jpg', show=True, conf=0.4, save=True) # if you want to save the inference in your device
 
 print(results)
